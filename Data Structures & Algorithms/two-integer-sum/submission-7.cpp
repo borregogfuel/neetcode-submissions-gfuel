ba1ba43cc893c0a6971ee90e11fd21da4469bd8f
guillermogfuel <a01199519@tec.mx>
2026-06-13 19:48:38 -0600

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> options;
+
+        for (int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if (options.count(complement)){
+                return {options[complement], i};
+            }
+            options[nums[i]] = i;
+
+        }
+        return{};
+    }
+};
